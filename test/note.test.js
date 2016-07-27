158b5ac49f46c9347c99d02a9c711cfb75529e5b
Danielle H <daheber@gmail.com>
2016-07-27 15:20:43 -0700

    note schema and unit test

--- test/note.test.js
+++ test/note.test.js
@@ -3,4 +3,20 @@ const assert = require( 'chai' ).assert;
 
 describe('note model', ()=>{
 
+  it('errors when note.body is empty', done=>{
+    const note = new Note();
+    note.validate(err=>{
+      if(!err) done('body required error');
+      else done();
+    });
+  });
+
+  it('note defaults to not important', done=>{
+    const note = new Note({ title: 'testing', body: 'content here'});
+    note.validate(()=>{
+      assert.equal(note.important, false);
+      done();
+    });
+  });
+
 });
