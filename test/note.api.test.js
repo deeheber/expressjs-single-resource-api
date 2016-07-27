088a7129b50bf6b6bba622e85cc3a59048c0da23
Danielle H <daheber@gmail.com>
2016-07-27 16:31:15 -0700

    GET all and POST

--- test/note.api.test.js
+++ test/note.api.test.js
@@ -1,47 +1,56 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const assert = require('chai').assert;
-const app = require('../lib/app');
-const connection = require('../lib/setup-mongoose');
-
 chai.use(chaiHttp);
+
+const connection = require('../lib/setup-mongoose');
+const app = require('../lib/app');
+
 
 describe('note api', ()=>{
 
+  before(done => {
+
+    const name = 'notes';
+    connection.on( 'open', () => {
+      connection.db
+      .listCollections({ name })
+      .next( (err, collinfo) => {
+        if (!collinfo) return done();
+        connection.db.dropCollection( name, done );
+      });
+    });
+  });
+
   const request = chai.request(app);
 
-  // it('gets all notes', done=>{
-  //   request.get('/api/notes')
-  //     .end((error, response)=>{
-  //       if(error)return done(error);
-  //       assert.equal(response.text, JSON.stringify(data.notes));
-  //       done();
-  //     });
-  // });
-  //
-  // it('gets note 1', done=>{
-  //   request.get('/api/notes/1')
-  //     .end((error, response)=>{
-  //       if(error)return done(error);
-  //       assert.equal(response.text, JSON.stringify(data.notes[0]));
-  //       done();
-  //     });
-  // });
-  //
-  // it('errors on a note id that is not a number', done=>{
-  //   request.get('/api/notes/foo')
-  //     .end((error)=>{
-  //       assert.equal(error.status, 400);
-  //       done();
-  //     });
-  // });
-  //
-  // it('errors on a note that does not exist', done=>{
-  //   request.get('/api/notes/400')
-  //     .end((error)=>{
-  //       assert.equal(error.status, 400);
-  //       done();
-  //     });
-  // });
+  const note1 = {
+    title: 'note1',
+    body: 'note 1 body',
+    important: true
+  };
 
+  it('gets all notes', done=>{
+    request.get('/api/notes')
+      .then(res => {
+        assert.deepEqual(res.body, []);
+        done();
+      })
+      .catch(done);
+  });
+
+  it('adds note1', done=>{
+    request.post('/api/notes')
+      .send(note1)
+      .then(res =>{
+        const note = res.body;
+        assert.ok(note._id);
+        note1.__v = 0;
+        note1._id = note._id;
+        done();
+      })
+      .catch(done);
+  });
+
+  after(done => connection.close(done));
 });
